0a23ed28afa496800f2116adfadf97713e9f4ba0
Gurkengewuerz <niklas@schuetrumpf.net>
2024-10-25 18:24:50 +0200

    feat: rewrite index to leaderboard

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,13 +1,14 @@
 package api
 
 import (
+	"github.com/gofiber/fiber/v2/middleware/rewrite"
 	"github.com/gurkengewuerz/GitCodeJudge/config"
 	"github.com/gurkengewuerz/GitCodeJudge/internal/api/handlers"
 	"github.com/gurkengewuerz/GitCodeJudge/internal/api/middleware"
 	"github.com/gurkengewuerz/GitCodeJudge/internal/judge"
-    "github.com/gurkengewuerz/GitCodeJudge/internal/judge/scoreboard"
+	"github.com/gurkengewuerz/GitCodeJudge/internal/judge/scoreboard"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
@@ -18,6 +19,12 @@ func SetupRouter(cfg *config.Config, pool *judge.Pool, scoreboardManager *scoreb
 	// Middleware
 	app.Use(logger.New())
 	app.Use(recover.New())
+
+	app.Use(rewrite.New(rewrite.Config{
+		Rules: map[string]string{
+			"/": "/leaderboard",
+		},
+	}))
 
 	// Routes
 	app.Get("/health", handlers.HealthCheck)
@@ -31,7 +38,7 @@ func SetupRouter(cfg *config.Config, pool *judge.Pool, scoreboardManager *scoreb
 	// commit results
 	app.Get("/results/:commit", handlers.HandleCommitResults())
 
-    // Scoreboard
+	// Scoreboard
 	app.Get("/user/:username", handlers.HandleUserProgress(scoreboardManager))
 	app.Get("/workshop/:workshop/:task", handlers.HandleWorkshopStats(scoreboardManager))
 	app.Get("/leaderboard", handlers.HandleLeaderboard(scoreboardManager))
